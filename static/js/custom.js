d7d48f5be6ab69acfc03af1ea1fb6f2f3ca6e051
guidogr95 <guido.gr@hotmail.com>
2019-11-02 18:24:01 -0500

    12

--- static/js/custom.js
+++ static/js/custom.js
@@ -230,7 +230,9 @@ $(window).bind('load', function() {
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
-      },
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+      }},
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
